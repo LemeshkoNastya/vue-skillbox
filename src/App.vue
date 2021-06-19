f2928edd7a217eceabe0faba366742a56eebbe3d
LemeshkoNastya <anastasia.lemeshko23@gmail.com>
2021-06-19 15:52:14 +0400

    feat: add task 4.4 and 4.5(1-2)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 /* eslint-disable import/extensions */
 <template>
-  <section class="catalog">
-    <ProductList :products="products"/>
-    <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage" />
-  </section>
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">
+        Каталог
+      </h1>
+      <span class="content__info">
+        152 товара
+      </span>
+    </div>
+
+    <div class="content__catalog">
+      <ProductFilter :price-from.sync="filterPriceFrom" :price-to.sync="filterPriceTo"
+      :category-id.sync="filterCategoryId" />
+
+      <section class="catalog">
+        <ProductList :products="products"/>
+        <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage" />
+      </section>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -12,24 +28,48 @@ import products from './data/products';
 import ProductList from './components/ProductList';
 // eslint-disable-next-line import/extensions
 import BasePagination from './components/BasePagination';
+// eslint-disable-next-line import/extensions
+import ProductFilter from './components/ProductFilter';
 
 export default {
   name: 'App',
-  components: { ProductList, BasePagination },
+  components: { ProductList, BasePagination, ProductFilter },
   data() {
     return {
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 0,
       page: 1,
       productsPerPage: 3,
     };
   },
   computed: {
+    filterProducts() {
+      let filteredProducts = products;
+
+      if (this.filterPriceFrom > 0) {
+        // eslint-disable-next-line max-len
+        filteredProducts = filteredProducts.filter((product) => product.price > this.filterPriceFrom);
+      }
+
+      if (this.filterPriceTo > 0) {
+        filteredProducts = filteredProducts.filter((product) => product.price < this.filterPriceTo);
+      }
+
+      if (this.filterCategoryId) {
+        // eslint-disable-next-line max-len
+        filteredProducts = filteredProducts.filter((product) => product.categoryId === this.filterCategoryId);
+      }
+
+      return filteredProducts;
+    },
     // eslint-disable-next-line vue/no-dupe-keys
     products() {
       const offset = (this.page - 1) * this.productsPerPage;
-      return products.slice(offset, offset + this.productsPerPage);
+      return this.filterProducts.slice(offset, offset + this.productsPerPage);
     },
     countProducts() {
-      return products.length;
+      return this.filterProducts.length;
     },
   },
 };
